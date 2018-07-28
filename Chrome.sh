07643f69e8ccb3127775fc3a1e9e4b1046cf7bfe
Aby Mathew <vabym8@gmail.com>
2018-07-28 15:24:20 +0300

    Update Chrome.sh

--- Chrome.sh
+++ Chrome.sh
@@ -1,6 +1,5 @@
-/*
-Open a chrome tab, Without address bar and bookmarkbar.
-replace URL.
-*/
+
+# This is a comment!Open a chrome tab, Without address bar and bookmarkbar.
+# This is a comment!replace URL.
 
 /Applications/Google\ Chrome.app/Contents/MacOS/Google\ Chrome --app=http://www.google.com/
